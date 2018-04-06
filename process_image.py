45f7223082d03983b5d8fc102273e5c5b3ddc39c
jonathanchamberlain <jdchambo@bu.edu>
2018-04-05 22:06:27 -0400

    Update process_image.py
    Updated methods to include quality factor, convert RGB data to YCC

--- process_image.py
+++ process_image.py
@@ -3,7 +3,8 @@ Process_image.py - takes as input 16x16x3 matrix, corresponding to a Macroblock 
 Outputs the result of the DCT transformation and Quantization
 Inputs - M, the Macroblock
 		 Q, the image compression quality, used in quanitzation
-Output - O, the Macroblock following transformation and Quantization
+Output - B, collection of blocks followign transformation/quantization
+Written by Jonathan Chamberlain - jdchambo@bu.edu
 '''
 
 import numpy
@@ -45,68 +46,82 @@ def dct2(y):
     return b
 
 '''
-Image processing begins
+Beginning of functions written for process_image
 '''
 
-# M = sys.argv[1]
-# Q = sys.argv[2]
-
-M = numpy.ones(shape=(16,16,3))
-Q = 100
+M = sys.argv[1]
+Q = sys.argv[2]
 
 # define individual blocks B1, B2, B3, B4, S.t. M is subdivided as
 # B1 | B2
 # _  _ _
 # B3 | B4
-B1 = numpy.zeros(shape=(8,8,3))
-B2 = numpy.zeros(shape=(8,8,3))
-B3 = numpy.zeros(shape=(8,8,3))
-B4 = numpy.zeros(shape=(8,8,3))
 
-# populate B1, B2, B3, B4 with image data
-# data stored as RGB, need YCbCr for MPEG-1 processing, convert // later step
+# These blocks will contain the Y' component of the coloration of the pixel
+B1 = numpy.zeros(shape=(8,8))
+B2 = numpy.zeros(shape=(8,8))
+B3 = numpy.zeros(shape=(8,8))
+B4 = numpy.zeros(shape=(8,8))
 
-B1 = M[0:8,0:8,:]
-B2 = M[0:8,8:16,:]
-B3 = M[8:16,0:8,:]
-B4 = M[8:16,0:8,:]
+# We subsample the Rb, Rr components to compress data - take data from B1 block only 
+Bb = numpy.zeros(shape=(8,8))
+Br = numpy.zeros(shape=(8,8))
+# populate B1, B2, B3, B4, Bb, Br with image data
+# data in M stored as RGB, convert to Y'RbRr - use JPEG standard
+
+R = M[:,:,0]
+G = M[:,:,1]
+B = M[:,:,2]
+
+for i in range(0,8):
+	for j in range(0,8):
+		B1[i,j] = (0.299*R[i,j])+(0.587*G[i,j])+(0.114*B[i,j])
+		B2[i,j] = (0.299*R[i,j+8])+(0.587*G[i,j+8])+(0.114*B[i,j+8])
+		B3[i,j] = (0.299*R[i+8,j])+(0.587*G[i+8,j])+(0.114*B[i+8,j])
+		B4[i,j] = (0.299*R[i+8,j+8])+(0.587*G[i+8,j+8])+(0.114*B[i+8,j+8])
+		Bb[i,j] = 128 - (0.168736*R[i,j]) - (0.331264*G[i,j]) + (0.5*B[i,j])
+		Br[i,j] = 128 + (0.5*R[i,j]) - (0.418688*G[i,j])- (0.081312*B[i,j])
 
 # for each matrix layer, run 2D DCT
 # subsample layers 2,3 by only running 2D DCT on these layers for B1
 
 # for now, Y cooresponds to R layer, Rb to B layer, Rr to G layer
+print B1
+B1 = dct2(B1) 
+B2 = dct2(B2) 
+B3 = dct2(B3) 
+B4 = dct2(B4) 
+Bb = dct2(Bb) 
+Br = dct2(Br) 
 
-OY1 = dct2(B1[:,:,0]) 
-OY2 = dct2(B2[:,:,0]) 
-OY3 = dct2(B3[:,:,0]) 
-OY4 = dct2(B4[:,:,0]) 
-ORb = dct2(B1[:,:,1]) 
-ORr = dct2(B1[:,:,2]) 
-
+print B1
 '''
 Quantization steps for each matrix
 '''
 
 #define base quantization matrix - taken from IJG standard 
 Qb = numpy.matrix('16 11 10 16 24 40 51 61;12 12 14 19 26 58 60 55;14 13 16 24 40 57 69 56;14 17 22 29 51 87 80 62;18 22 37 56 68 109 103 77;24 35 55 64 81 104 113 92;49 64 78 87 103 121 120 101;72 92 95 98 112 100 103 99') 
-'''
+
 # from quaity factor, determine multiplication factor for quantization
 if Q < 50:
 	S = 5000/Q;
 else:
 	S = 200 - 2*Q;
-'''
+	
+
 
 # determine quantized matrix result
 for i in range (0,8):
 	for j in range (0,8):
-		Qf = Qb[i,j]
-		OY1[i,j] = OY1[i,j] // Qf
-		OY2[i,j] = OY2[i,j] // Qf
-		OY3[i,j] = OY3[i,j] // Qf
-		OY4[i,j] = OY4[i,j] // Qf
-		ORb[i,j] = ORb[i,j] // Qf
-		ORr[i,j] = ORr[i,j] // Qf
+		Qf = (S*Qb[i,j] + 50) // 100
+		if Qf == 0:
+			Qf = 1
+		B1[i,j] = B1[i,j] // Qf
+		B2[i,j] = B2[i,j] // Qf
+		B3[i,j] = B3[i,j] // Qf
+		B4[i,j] = B4[i,j] // Qf
+		Bb[i,j] = Bb[i,j] // Qf
+		Br[i,j] = Br[i,j] // Qf
 		
-
-
+# define the collection of the 6 processed blocks, and return
+B = [B1, B2, B3, B4, Bb, Br]

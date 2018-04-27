210ac3999dd9e3dd00e120d57e1e4615169c386f
jonathanchamberlain <jdchambo@bu.edu>
2018-04-27 16:06:44 -0400

    Add files via upload

--- encode_image.py
+++ encode_image.py
@@ -1,7 +1,7 @@
 '''
 encode_image.py - takes individual images, specified QF factor, and output file, and writes the encoded image to the output
-Input - image, the image being encoded
-		output, the output file being appended to
+Input - fimage, the image being encoded
+		foutput, the output file being appended to
 		QF - the quantization factor, used in encoding to suppress high frequencies
 Output - none, encoding is written to file
 Written by Jonathan Chamberlain - jdchambo@bu.edu
@@ -9,11 +9,28 @@ Written by Jonathan Chamberlain - jdchambo@bu.edu
 
 import sys
 import proto_mpeg
+import cv2
+import numpy as np
 
 # parse inputs
 
-image = sys.argv[1]
-output = sys.argv[2]
+fimage = sys.argv[1]
+foutput = sys.argv[2]
 QF = sys.argv[3]
 
-# 
+# take image, convert to proto_mpeg Frame type
+image = proto_mpeg.Frame(cv2.imread(fimage))
+
+# open output file
+
+# starting from top left corner, take each MB and encode
+for vert in range(0,image.v_mblocks):
+	for hor in range(0,image.h_mblocks):
+		MB = image.getBlock(vert, hor)
+		encoded = image.encodeBlock(MB,QF)
+		#write encoded to output file
+		
+
+# close output file
+	
+		

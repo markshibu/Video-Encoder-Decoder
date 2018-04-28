f52e613d2820b664a226b001914e19b46b09a6a7
jefflin96 <jlin96@bu.edu>
2018-04-28 13:05:36 -0400

    More robust gui

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,45 +1,116 @@
-import tkinter as tk
-from tkinter.ttk import *
+from tkinter import *
+from tkinter import ttk
+import time
 
-main = tk.Tk()
+main = Tk()
 main.geometry('500x500')
-FileList = tk.Listbox(main, width = 40, height = 20)
-#FileList.
-i = 1
+progBarVal = 0
 
-# class customListBox(tk.Listbox):
-# 	def __init__(self):
-# 		self.pack()
+class myListbox(Listbox):
+	def __init__(self, frame, **kwargs):
+		kwargs['selectmode'] = tkinter.SINGLE
+		self.currIndex = None
 
-
-def say_hi():
-	for x in FileList:
-		print(x)
-
+################
+#   FUNCTIONS
+################
 def add():
-	FileList.insert(tk.END,1)
-	print("insert")
+	FileList.insert(END,1)
+	#print("insert")
+
+def remove():
+	file = FileList.curselection()
+	if file != ():
+		fileIndex = int(file[0])
+		FileList.delete(fileIndex,fileIndex)
+		#print("remove at ",fileIndex)
+
+def clear():
+	FileList.delete(0,END)
+
+
+#NEED TO IMPLEMENT ENCODING AND DECODING FUNCTIONS
+def encoding():
+	fileName = outputFile.get()
+	if fileName == "":
+		fileName = "output.bin"
+	print(fileName)
 	
 
-def delete():
-	FileList.delete(0,tk.END)
+def decoding(): 
+	print("IM DECODING")
 
-frame = Frame(master=main)
 
-hi_there = tk.Button(frame, text = "Hello World\n(clickme)", command = say_hi)
-hi_there.pack(side = "left")
+###############
+#    FRAMES
+###############
 
-add = tk.Button(frame,text="ADD", command = add())
-add.pack(side = "bottom")
+fileFrame = Frame(master = main)
+fileButtonFrame = Frame(master = fileFrame)
+quitFrame = Frame(master = main)
+encodeDecodeFrame = Frame(master = main)
+outputFrame = Frame(master = main)
+progressFrame = Frame(master = main)
 
-delete = tk.Button(frame, text = "DELETE", command = delete())
-delete.pack(side = "bottom")
 
-quit = tk.Button(frame, text="QUIT", fg = "white", bg="red", command = main.destroy)
-quit.pack(side = "right")
+###############
+#	OBJECTS
+###############
 
-FileList.pack(side = "top")
-frame.pack(side = "bottom")
+#LIST BOX
+FileList = Listbox(fileFrame,width = 50, height = 15)
 
+
+#ADD/REMOVE FILE BUTTONS
+
+addFileButton = Button(fileButtonFrame,text="Add File", command = add)
+removeFileButton = Button(fileButtonFrame, text = "Remove File", command = remove)
+clearAllButton = Button(fileButtonFrame, text = "Clear All", command = clear)
+
+addFileButton.pack(side = TOP)
+removeFileButton.pack(side = TOP)
+clearAllButton.pack(side = TOP)
+
+
+#OUTPUT ENTRY
+outputLabel = Label(outputFrame, justify='left', text='Output File Name:')
+outputFile = Entry(outputFrame, width = 40)
+outputFile.delete(0,END)
+
+outputLabel.pack(side = LEFT)
+outputFile.pack(side = LEFT)
+
+#ENCODE DECODE BUTTONS
+encodeButton = Button(encodeDecodeFrame,text="Encode Files", command = encoding)
+decodeButton = Button(encodeDecodeFrame, text="Decode File", command = decoding)
+
+decodeButton.pack(side = RIGHT)
+encodeButton.pack(side = RIGHT)
+
+#PROGRESS BAR
+progBarLabel = Label(progressFrame, justify='left', text='Progress')
+progBar = ttk.Progressbar(progressFrame, orient = "horizontal", length = 400, mode = "determinate",\
+		maximum = 100, value = 0, variable = progBarVal)
+
+progBarLabel.pack(side=TOP)
+progBar.pack(fill = X, pady = 3)
+
+
+#QUIT BUTTON
+quitButton = Button(quitFrame, text="QUIT", fg = "white", bg="red", command = main.destroy)
+quitButton.pack(side = RIGHT)
+
+
+#FRAME POSITIONING
+fileFrame.pack(side = TOP, pady = 5)
+FileList.pack(side = LEFT)
+fileButtonFrame.pack(side = RIGHT)
+
+outputFrame.pack(side = TOP, pady = 5)
+encodeDecodeFrame.pack(side = TOP)
+progressFrame.pack(side = TOP, pady = 5)
+quitFrame.pack(side = TOP)
+
+main.title("EC504 Video Encoder/Decoder")
 
 main.mainloop()
